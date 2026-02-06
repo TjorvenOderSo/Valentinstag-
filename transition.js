3add634ae093f3d3dfc21fdd0ee07ab4c63a7359
TjorvenOderSo <gross.tjorven@gmail.com>
2026-02-06 09:57:41 +0100

    Create transition.js

---
+++ transition.js
@@ -0,0 +1,21 @@
+function startGame() {
+  let count = 0;
+
+  let interval = setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerText = "💗";
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    document.body.appendChild(heart);
+
+    setTimeout(() => heart.remove(), 2000);
+
+    count++;
+    if (count > 40) {
+      clearInterval(interval);
+      setTimeout(() => {
+        window.location.href = "game.html";
+      }, 1000);
+    }
+  }, 100);
+} 
